e3a912061ac03e6fe482b111c47e36d657aeb2e2
callmerey <levii.reys@gmail.com>
2020-12-09 14:03:31 +0700

    index

---
+++ lib/src/main/java/lib_book/lib/controller/DemoController.java
@@ -0,0 +1,62 @@
+package lib_book.lib.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DemoController {
+    @RequestMapping("/index")
+    public String index(){
+        return "index/index";
+    }
+
+    @RequestMapping("/list")
+    public String list(){
+        return "index/list";
+    }
+
+    @RequestMapping("/weapon")
+    public String weapon(){
+        return "index/weapon";
+    }
+
+    @RequestMapping("/detail")
+    public String detail(){
+        return "layouts/detail";
+    }
+
+    @RequestMapping("/monster")
+    public String monster(){
+        return "index/monster";
+    }
+
+    @RequestMapping("/npc")
+    public String npc(){
+        return "index/npc";
+    }
+
+    @RequestMapping("/map")
+    public String map(){
+        return "index/map";
+    }
+
+    @RequestMapping("/use")
+    public String use(){
+        return "index/use";
+    }
+
+    @RequestMapping("/skill")
+    public String skill(){
+        return "index/skill";
+    }
+
+    @RequestMapping("/quest")
+    public String quest(){
+        return "index/quest";
+    }
+
+    @RequestMapping("/exp")
+    public String exp(){
+        return "index/exptable";
+    }
+}
---
+++ lib/src/main/java/lib_book/lib/util/WebConfig.java
@@ -0,0 +1,16 @@
+package lib_book.lib.util;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer{
+    
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**","/js/**","/images/**").addResourceLocations("classpath:/static/css/","classpath:/static/js/","classpath:/static/images/");
+    }
+}
